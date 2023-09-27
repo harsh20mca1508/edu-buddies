0567e4f1f09dcf3ca8ddd75581f8eb957c93fba4
Harsh Kapil <kapilharsh1999@gmail.com>
2023-09-27 22:43:06 +0530

    final

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const Logo = () => {
+    return (
+        <Image
+            src="/edubuddies.svg"
+            alt="Logo image"
+            height={200}
+            width={200}
+            
+        />
+    )
+}
---
+++ lib/format.ts
@@ -0,0 +1,8 @@
+import { Currency } from "lucide-react"
+
+export const formatPrice = (price: number) => {
+    return new Intl.NumberFormat("en-us", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+}
